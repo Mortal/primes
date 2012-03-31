38675e5617f5c968f98fd1952cb310355ba32ec8
Mathias Rav <rav@cs.au.dk>
2012-03-31 17:54:12 +0200

    Use boost::timer to provide calculation times

--- primes.cpp
+++ primes.cpp
@@ -1,21 +1,36 @@
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <cstdio>
+#include <boost/timer.hpp>
+
+struct log {
+  boost::timer t;
+  std::ostream & operator()() {
+    return std::cout << '[' << std::setw(7) << std::fixed << std::setprecision(2) << t.elapsed() << ']' << ' ';
+  }
+};
 
 int main() {
-  const size_t n = 1 << 30;
-  const size_t every = 1 << 14;
+  const unsigned int n = 1 << 31;
   std::vector<bool> notprime(n);
   notprime[0] = notprime[1] = true;
+  unsigned int threshold = 4;
+  log l;
+  l() << "Finding all primes less than " << n << std::endl;
   for (unsigned int i = 2; i < (n); ++i) {
+    if (i == threshold) {
+      l() << "Found all primes less than " << threshold << std::endl;
+      threshold = threshold << 1;
+    }
     if (!notprime[i]) {
       for (unsigned int j = i+i; j < (n); j += i) {
 	notprime[j] = true;
       }
     }
   }
-  std::cout << "Calculated all the primes!" << std::endl;
+  l() << "Found all primes less than " << n << ", printing to primes.bin" << std::endl;
   FILE * primes = fopen("primes.bin", "w+");
   for (size_t offset = 0; offset < n;) {
     size_t limit = offset+256;
@@ -30,11 +45,14 @@ int main() {
       }
       ++offset, ++i;
     }
-    if (count == 0) std::cout << offset << std::endl;
+    if (count == 0) {
+      l() << "There are no primes in the range " << limit-256 << " to " << limit-1 << std::endl;
+    }
     fseek(primes, pos, SEEK_SET);
     fwrite(&count, 1, 1, primes);
     fseek(primes, 0, SEEK_END);
   }
   fclose(primes);
+  l() << "Finished writing" << std::endl;
   return 0;
 }
